6de44c7fae3cc1ff1950225dfee020cb91f9cecb
HoleInOneGolfer <benjamin.d.bowles@gmail.com>
2026-07-18 15:28:01 -0500

    misc changes

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # GMTK-2026
+
 My entry into the 2026 Game Makers Toolkit Game Jam.
--- scripts/autoload/scene_manager.gd
+++ scripts/autoload/scene_manager.gd
@@ -1,4 +1,6 @@
 extends Node
 
 func change_scene(path: String):
+	# This just swaps the scene directly. 
+	# You can add transition effects here later.
 	get_tree().change_scene_to_file(path)
