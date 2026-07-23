1274d15f53c27b27adede28947993ca9a5d297a9
HoleInOneGolfer <benjamin.d.bowles@gmail.com>
2026-07-22 20:58:18 -0500

    Added More Collectibles and art to them

--- scripts/collectible.gd
+++ scripts/collectible.gd
@@ -1,12 +1,24 @@
+class_name Collectible
 extends Area2D
 
 enum Type { TREASURE, HAZARD }
-@export var item_type: Type = Type.TREASURE
-@export var point_value: int = 10
+
+@export var possible_items: Array[CollectibleData] = []
+
+@onready var sprite_2d: Sprite2D = $Sprite2D # Make sure your child node name matches
+
+var item_type: Type = Type.TREASURE
+var point_value: int = 10
 
 func _ready() -> void:
 	add_to_group("collectibles")
 	body_entered.connect(_on_body_entered)
+	
+	if possible_items.size() > 0:
+		var chosen_item = possible_items.pick_random()
+		item_type = chosen_item.item_type
+		point_value = chosen_item.point_value
+		sprite_2d.texture = chosen_item.texture
 
 func _on_body_entered(body: Node2D) -> void:
 	if item_type == Type.HAZARD:
---
+++ scripts/collectibleData.gd
@@ -0,0 +1,7 @@
+# CollectibleData.gd
+class_name CollectibleData
+extends Resource
+
+@export var texture: Texture2D
+@export var item_type: Collectible.Type
+@export var point_value: int
